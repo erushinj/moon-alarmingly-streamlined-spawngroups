c32cafc8ab4f3160f5a33e240b5cd999384c71f4
moon <108492539+erushinj@users.noreply.github.com>
2022-11-07 17:45:49 -0600

    Remove hurt tweaks
    They don't sync.

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -58,111 +58,6 @@ function CharacterTweakData:_presets(tweak_data, ...)
 		}
 	}
 
-	presets.hurt_severities.base.melee.zones = {
-		{
-			health_limit = 0.2,
-			light = 1
-		},
-		{
-			health_limit = 0.5,
-			light = 0.5,
-			moderate = 0.5
-		},
-		{
-			health_limit = 0.8,
-			moderate = 0.5,
-			heavy = 0.5
-		},
-		{
-			heavy = 1
-		}
-	}
-	presets.hurt_severities.base.explosion.zones = {
-		-- {	--	testing
-		-- 	health_limit = 0.2,
-		-- 	light = 1
-		-- },
-		{
-			health_limit = 0.2,
-			light = 0.5,
-			moderate = 0.5
-		},
-		{
-			health_limit = 0.5,
-			moderate = 0.5,
-			heavy = 0.5
-		},
-		{
-			health_limit = 0.8,
-			heavy = 0.5,
-			explode = 0.5
-		},
-		{
-			explode = 1
-		}
-	}
-
-	presets.hurt_severities.light_hurt_fire_poison.explosion = presets.hurt_severities.base.explosion
-
-	presets.hurt_severities.no_heavy_hurt = deep_clone(presets.hurt_severities.base)
-	presets.hurt_severities.no_heavy_hurt.bullet.zones = {
-		{
-			health_limit = 0.4,
-			none = 0.5,
-			light = 0.5
-		},
-		{
-			health_limit = 0.7,
-			light = 0.7,
-			moderate = 0.3
-		},
-		{
-			light = 0.5,
-			moderate = 0.5
-		}
-	}
-	presets.hurt_severities.no_heavy_hurt.melee.zones = {
-		{
-			health_limit = 0.2,
-			none = 0.2,
-			light = 0.8
-		},
-		{
-			health_limit = 0.5,
-			light = 0.8,
-			moderate = 0.2
-		},
-		{
-			health_limit = 0.8,
-			light = 0.2,
-			moderate = 0.8
-		},
-		{
-			moderate = 0.8,
-			heavy = 0.2
-		}
-	}
-	presets.hurt_severities.no_heavy_hurt.explosion.zones = {
-		{
-			health_limit = 0.2,
-			light = 1,
-		},
-		{
-			health_limit = 0.5,
-			light = 0.5,
-			moderate = 0.5
-		},
-		{
-			health_limit = 0.8,
-			moderate = 0.5,
-			heavy = 0.5
-		},
-		{
-			heavy = 0.5,
-			explode = 0.5
-		}
-	}
-
 	return presets
 end
 
@@ -177,23 +72,8 @@ Hooks:PostHook( CharacterTweakData, "init", "ass_init", function(self)
 	self.fbi_heavy_swat.surrender = self.presets.surrender.hard
 	self.heavy_swat_sniper.surrender = self.presets.surrender.hard
 
-	self.heavy_swat.damage.hurt_severity = self.presets.hurt_severities.no_heavy_hurt
-	self.fbi_heavy_swat.damage.hurt_severity = self.presets.hurt_severities.no_heavy_hurt
-	self.heavy_swat_sniper.damage.hurt_severity = self.presets.hurt_severities.no_heavy_hurt
-
-	self.marshal_marksman.damage.hurt_severity = self.presets.hurt_severities.light_hurt_fire_poison
 	self.marshal_marksman.misses_first_player_shot = false
-
-	self.taser.damage.hurt_severity = self.presets.hurt_severities.light_hurt_fire_poison
 
 	self.medic.suppression = nil
 
 end )
-
-
-local function city_swat_hurts(char_tweak_data)
-	char_tweak_data.city_swat.damage.hurt_severity = char_tweak_data.presets.hurt_severities.base
-end
-Hooks:PostHook( CharacterTweakData, "_set_easy_wish", "ass__set_easy_wish", city_swat_hurts )
-Hooks:PostHook( CharacterTweakData, "_set_overkill_290", "ass__set_overkill_290", city_swat_hurts )
-Hooks:PostHook( CharacterTweakData, "_set_sm_wish", "ass__set_sm_wish", city_swat_hurts )
