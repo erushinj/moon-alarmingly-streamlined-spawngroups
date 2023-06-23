cdcb1d9a2148a13279097faa5982f07537ef59c2
moon <108492539+erushinj@users.noreply.github.com>
2023-06-22 21:24:26 -0600

    Tweak Watchdogs day 2

--- req/mission_script/watchdogs_2.lua
+++ req/mission_script/watchdogs_2.lua
@@ -1,5 +1,4 @@
 local units = ASS:base_units()
-local is_easy_wish = ASS:is_difficulty_at_least("easy_wish")
 
 return {
 	-- dozer with 4 shields guarding bags, goes up to minigun now
@@ -7,6 +6,6 @@ return {
 	[102375] = { enemy = ASS:get_difficulty_dozer(ASS.DOZER_TIERS.MINI) },
 
 	-- helicopter spawn in front of the warehouse
-	[100446] = { enemy = is_easy_wish and units.taser or units.medic_1 },
-	[100447] = { enemy = is_easy_wish and ASS:get_difficulty_dozer(ASS.DOZER_TIERS.MEDIC) or units.taser },
+	[100446] = { enemy = ASS:random_unit("specials_med") },
+	[100447] = { enemy = ASS:is_difficulty_at_least("easy_wish") and ASS:get_difficulty_dozer(ASS.DOZER_TIERS.MINI) or units.taser },
 }
