b0f10981c383dc1e46297460dc753dfe39620fd1
moon <108492539+erushinj@users.noreply.github.com>
2024-05-13 21:11:02 -0600

    Reduce interval on single_spooc group spawn points

--- lua/elementspawnenemygroup.lua
+++ lua/elementspawnenemygroup.lua
@@ -23,3 +23,20 @@ end
 
 -- allow regular cloaker groups to spawn from manholes/vents/etc, since the cloaker task has been turned off
 group_mapping.single_spooc = check_clone(group_mapping.FBI_spoocs, true)
+
+-- reduce spawn point intervals on single_spooc spawns with only one spawn point to compensate for groups with 2 units
+ASS:pre_hook( ElementSpawnEnemyGroup, "_finalize_values", function(self)
+	local preferred = self._values.preferred_spawn_groups
+
+	if preferred and #preferred == 1 and preferred[1] == "single_spooc" then
+		local elements = self._values.elements
+
+		if elements and #elements == 1 then
+			local spawn = self:get_mission_element(elements[1])
+
+			if spawn then
+				spawn._values.interval = math.min(spawn._values.interval or 1, 1)
+			end
+		end
+	end
+end )
