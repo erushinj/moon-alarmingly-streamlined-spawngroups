203691e5e867a0339489128cd45808947785c164
moon <108492539+erushinj@users.noreply.github.com>
2024-08-29 12:55:24 -0600

    Slight rearranging in charactertweakdata

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -2,9 +2,6 @@ if ASS.is_client then
 	return
 end
 
-local difficulty_index = ASS.difficulty_index
-local f = (difficulty_index - 2) / 6
-
 function CharacterTweakData:moon_oops_all_bo(is_first_world_problem)
 	local flee_type = self.civilian.flee_type
 	local run_away_delay = self.civilian.run_away_delay
@@ -22,47 +19,42 @@ end
 if ASS:setting("doms_scale") then
 	ASS:log("info", "Resistive Responders setting enabled, tweaking \"CharacterTweakData:_presets\"...")
 
+	-- only change SH's tweaked surrender presets
+	-- allow any factor to count as a surrender reason
+	local f = (ASS.difficulty_index - 2) / 6
 	Hooks:PostHook( CharacterTweakData, "_presets", "ass__presets", function()
-		local presets = Hooks:GetReturn()
+		for _, preset in pairs(Hooks:GetReturn().surrender) do
+			if preset.reasons and preset.factors and preset.factors.health then
+				local min, max = math.min_max(preset.significant_chance or 0, 0.5)
 
-		if not presets then
-			ASS:log("warn", "\"CharacterTweakData:_presets\" unavailable!")
-		else
-			-- only change SH's tweaked surrender presets
-			-- allow any factor to count as a surrender reason
-			for _, preset in pairs(presets.surrender) do
-				if preset.reasons and preset.factors and preset.factors.health then
-					local min, max = math.min_max(preset.significant_chance or 0, 0.5)
-
-					preset.significant_chance = math.lerp(min, max, f)
-					preset.base_chance = 0
-					preset.factors_original = preset.factors
-					preset.reasons = table.map_append(preset.reasons, preset.factors)
-					preset.factors = {}
-				end
+				preset.significant_chance = math.lerp(min, max, f)
+				preset.base_chance = 0
+				preset.factors_original = preset.factors
+				preset.reasons = table.map_append(preset.reasons, preset.factors)
+				preset.factors = {}
 			end
 		end
-
-		return presets
 	end )
 end
 
-local doms_all_hard = ASS:setting("doms_all_hard")
-Hooks:PostHook( CharacterTweakData, "init", "ass_init", function(self)
-	if doms_all_hard then
-		ASS:log("info", "Difficult Dominations setting enabled, changing assigned surrender presets...")
+if ASS:setting("doms_all_hard") then
+	ASS:log("info", "Difficult Dominations setting enabled, changing assigned surrender presets...")
 
+	-- swap easy and normal preset for hard preset
+	-- could replace their data in _presets but i dont really like that
+	Hooks:PostHook( CharacterTweakData, "init", "ass_init", function(self)
 		local surrender_map = {
 			[self.presets.surrender.easy] = self.presets.surrender.hard,
 			[self.presets.surrender.normal] = self.presets.surrender.hard,
 		}
+
 		for _, data in pairs(self) do
 			if type(data) == "table" then
 				data.surrender = surrender_map[data.surrender] or data.surrender
 			end
 		end
-	end
-end )
+	end )
+end
 
 -- add missing custom heist units to the character map
 -- some of them go to copbase to add materials directly, but this does not work for my purposes
